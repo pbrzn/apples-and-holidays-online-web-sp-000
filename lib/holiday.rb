51c6ec36885e7b4078b1ccdc65a53724fbf3f7fa
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-03-21 00:40:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,6 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
     end
   end
 end
+binding.pry
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
