4d448ee053791a90756d115964be5ddbacbf7666
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-03-21 01:09:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,11 +72,13 @@ def all_supplies_in_holidays(holiday_hash)
     end
   end
 end
-#binding.pry
+
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  holiday_hash.each do |season, holiday|
+    
 
 end
 
